47ded81d11566aac1c15b56ad76a37ba087ad22e
Serban Ghita <serbanghita@gmail.com>
2022-09-27 23:27:28 +0300

    WIP demo.

--- packages/glhf-ecs/src/mocks/stubs.ts
+++ packages/glhf-ecs/src/mocks/stubs.ts
@@ -4,7 +4,7 @@ export function render(entity: Entity) {
     console.log('render', entity);
 }
 
-export function loop(now: DOMHighResTimeStamp) {
+export function loop(now: () => void) {
     console.log('loop', now);
     window.requestAnimationFrame(loop);
 }

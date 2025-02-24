28ec8394be1671400b11d350c61d1476c35f44b7
Serban Ghita <serbanghita@gmail.com>
2025-02-24 12:52:18 +0200

    Explained the demo

--- packages/demos/quadtree-dynamic-points/src/demo.ts
+++ packages/demos/quadtree-dynamic-points/src/demo.ts
@@ -2,34 +2,47 @@ import { createCanvas, createWrapperElement, rectangle, circle, dot, run } from 
 import { QuadTree } from "@serbanghita-gamedev/quadtree";
 import { Rectangle, Point } from "@serbanghita-gamedev/geometry";
 
+/**
+ * This is a demo of a Quad Tree used for rudimentary collision detection.
+ * All points are spawned and move randomly, while you move with the mouse over them and
+ * "collide" with them.
+ */
+
 export function randomInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/**
+ * Define the Quad Tree area.
+ */
 const area = new Rectangle(640, 480, new Point(640 / 2, 480 / 2));
 const quadtree = new QuadTree(area, 5, 10);
 
-const points: Point[] = [];
+const queryAreaCenterPoint = new Point(640 / 2, 480 / 2);
+const queryArea = new Rectangle(120, 120, queryAreaCenterPoint);
 
+/**
+ * Generate random Points for later rendering.
+ */
+const points: Point[] = [];
 for (let x = 0; x < 640 / 2; x += 3) {
   for (let y = 0; y < 480 / 2; y += 3) {
     points.push(new Point(x, y));
   }
 }
 
-const queryAreaCenterPoint = new Point(640 / 2, 480 / 2);
-const queryArea = new Rectangle(120, 120, queryAreaCenterPoint);
-
-/******************************************************************
- * Render for demo
- * ****************************************************************/
+/**
+ * Create the <canvas> element.
+ */
 const HTML_WRAPPER = createWrapperElement("game-wrapper", 640, 480);
 const CANVAS_BACKGROUND = createCanvas("background", 640, 480, "1");
 HTML_WRAPPER.appendChild(CANVAS_BACKGROUND);
 document.body.appendChild(HTML_WRAPPER);
-
 const ctx = CANVAS_BACKGROUND.getContext("2d") as CanvasRenderingContext2D;
 
+/**
+ * Recursive rendering method to render all the quadrants of the tree.
+ */
 function renderQuadTree(quadtree: QuadTree) {
   rectangle(ctx, quadtree.area.topLeftX, quadtree.area.topLeftY, quadtree.area.width, quadtree.area.height, "rgba(0,0,0,0.1)");
 
@@ -41,6 +54,10 @@ function renderQuadTree(quadtree: QuadTree) {
     renderQuadTree(subQuadtree);
   });
 }
+
+/**
+ * Create the mouse interaction.
+ */
 let allowDraw = false;
 
 CANVAS_BACKGROUND.addEventListener("mousedown", () => (allowDraw = true));
